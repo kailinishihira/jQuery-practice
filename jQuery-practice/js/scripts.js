5d576cbbdffc984838040f40f0f52a941bf274aa
Kaili Nishihira <kailinishihira@gmail.com>
2017-07-04 23:34:47 -0700

    Add toggle to walrus image.

--- jQuery-practice/js/scripts.js
+++ jQuery-practice/js/scripts.js
@@ -1,14 +1,6 @@
-jQuery(document).ready(function() {
-  jQuery("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
-  });
-
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  jQuery("img").click(function() {
-    alert("This is an image.");
-  });
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-hidden").toggle();
+    $("#walrus-showing").toggle();
+  })
 });
